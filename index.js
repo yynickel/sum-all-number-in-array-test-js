61e1f82423e7382ad4bffbeb147e7c02f8aec4da
Nick Yu <yynickel@gmail.com>
2019-06-28 02:59:11 +0000

    recursion is powerful

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  return array.reduce((acc, cur) => {
+    if (Array.isArray(cur)) {
+      return sumItems(cur) + acc;
+    } else {
+      return cur + acc;
+    }
+  }, 0);
 }
 
 module.exports = sumItems;
